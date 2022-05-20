b75643612cc8aa6337d7e8d6e97e9a3a7ee4c6ec
Mikolaj Rychel <mikolaj.rychel@enginiety.com>
2022-05-20 18:03:23 +0200

    Fix bugs

--- LoginAPI/userstore/urls.py
+++ LoginAPI/userstore/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from LoginAPI.userstore import views
 
 urlpatterns = [
-    path('user/', views.user),
-    path('user/<token>', views.user),
+    path('user', views.user),
+    path('user/activate', views.user_activate),
     path('user/passwd', views.user_passwd),
+    path('user/delete', views.user_delete),
 
-    path('token/', views.token),
+    path('token', views.token),
     path('token/refresh', views.token_refresh),
 ]
--- LoginAPI/userstore/views.py
+++ LoginAPI/userstore/views.py
@@ -1,18 +1,23 @@
 from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from LoginAPI import settings
+from django.utils import timezone
 from datetime import timedelta
 import json
 
+
 @csrf_exempt
-def user(request, token=None):
+def user(request):
     if request.method == "POST":
         email = request.POST.get('email')
         passwd = request.POST.get('passwd')
+
         try:
             User.objects.get(username=email)
-            return HttpResponse(json.dumps({"message": "User exists in database."}, indent=2,separators=(',', ': ')),
+            return HttpResponse(json.dumps({"message": "User exists in database."}, indent=2, separators=(',', ': ')),
                                 content_type="application/json", status=403)
         except:
             user = User.objects.create_user(username=email)
@@ -22,83 +27,91 @@ def user(request, token=None):
             user.save()
             return HttpResponse(json.dumps({"message": "User successfully created!"}, indent=2,separators=(',', ': ')),
                                 content_type="application/json", status=200)
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
 
-    if request.method == "DELETE":
-        email = request.POST.get('email')
-        passwd = request.POST.get('passwd')
-        try:
-            user = User.objects.get(username=email, password=passwd)
-            user.delete()
-        except:
-            return HttpResponse(json.dumps({"message": "User doesn't exist in database."}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=404)
 
-        return HttpResponse(json.dumps({"message": "User successfully deleted!"}, indent=2, separators=(',', ': ')),
-                            content_type="application/json", status=200)
-
-    if request.method == "GET":
+@csrf_exempt
+def user_delete(request):
+    if request.method == "POST":
+        token = request.POST.get('token')
         if token:
             try:
-                user = User.objects.get(id=token)
-                user.is_active = True
-                user.save()
-                return HttpResponse(json.dumps({"message": "User successfully activated!"}, indent=2, separators=(',', ': ')),
+                session = Session.objects.get(session_key=token)
+                uid = session.get_decoded().get('_auth_user_id')
+                user = User.objects.get(id=uid)
+                user.delete()
+                return HttpResponse(json.dumps({"message": "User successfully deleted!"}, indent=2, separators=(',', ': ')),
                                 content_type="application/json", status=200)
             except:
-                return HttpResponse(json.dumps({"message": "User already active!"}, indent=2, separators=(',', ': ')),
+                return HttpResponse(json.dumps({"message": "User doesn't exist!"}, indent=2, separators=(',', ': ')),
                                 content_type="application/json", status=400)
-        else:
-            return HttpResponse(json.dumps({"message": "Ivalid token"}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=404)
+
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
+
+
+@csrf_exempt
+def user_activate(request):
+    if request.method == "POST":
+        token = request.POST.get('token')
+
+        user = User.objects.get(id=token)
+        user.is_active = True
+        user.save()
+        return HttpResponse(json.dumps({"message": "User successfully activated!"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=200)
+
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
+
 
 @csrf_exempt
 def user_passwd(request):
-    if request.method == "PATCH":
-
-        email = request.POST.get('email')
-        old_passwd = request.POST.get('old_passwd')
-        new_passwd = request.POST.get('new_passwd')
+    if request.method == "POST":
+        token = request.POST.get('token')
+        passwd = request.POST.get('passwd')
         try:
-            User.objects.get(username=email, password=old_passwd)
-            return HttpResponse(json.dumps({"message": "Ivalid email or password"}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=404)
-        except:
-            user = User.objects.create_user(username=email)
-            user.email = email
-            user.set_password(new_passwd)
+            session = Session.objects.get(session_key=token)
+            uid = session.get_decoded().get('_auth_user_id')
+            user = User.objects.get(id=uid)
+            user.set_password(passwd)
             user.save()
-            return HttpResponse(json.dumps({"message": "Password sucessfully changed"}, indent=2, separators=(',', ': ')),
+            return HttpResponse(json.dumps({"message": "Password changed successfully!"}, indent=2, separators=(',', ': ')),
                                 content_type="application/json", status=200)
+        except:
+            print("")
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
 
 @csrf_exempt
 def token(request):
     if request.method == "POST":
         email = request.POST.get('email')
         passwd = request.POST.get('passwd')
-
-        try:
-            User.objects.get(username=email, password=passwd)
-            token = request.session._get_or_create_session_key()
-            token.set_expiry(timedelta(seconds=settings.SESSION_COOKIE_AGE))
-            return HttpResponse(json.dumps({"token": f"{token}"}, indent=2, separators=(',', ': ')),
+        user = authenticate(username=email, password=passwd)
+        if user:
+            login(request, user)
+            logout(request)
+            login(request, user)
+            token = request.session.session_key
+            return HttpResponse(json.dumps({"JWT_TOKEN": f"{token}"}, indent=2, separators=(',', ': ')),
                                 content_type="application/json", status=200)
-        except:
-            return HttpResponse(json.dumps({"message": "Ivalid email or password"}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=404)
+        return HttpResponse(json.dumps({"message": f"User not found"}, indent=2, separators=(',', ': ')),
+                                        content_type="application/json", status=404)
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
 
 @csrf_exempt
 def token_refresh(request):
-    if request.method == "PATCH":
+    if request.method == "POST":
+        token = request.POST.get('token')
 
-        email = request.POST.get('email')
-        passwd = request.POST.get('passwd')
-
-        try:
-            User.objects.get(username=email, password=passwd)
-            token = request.session._get_or_create_session_key()
-            token.set_expiry(timedelta(seconds=settings.SESSION_COOKIE_AGE))
-            return HttpResponse(json.dumps({"JWT_TOKEN": f"{token}"}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=200)
-        except:
-            return HttpResponse(json.dumps({"JWT_TOKEN": f"{token}"}, indent=2, separators=(',', ': ')),
-                                content_type="application/json", status=200)
+        session = Session.objects.get(session_key=token)
+        if session:
+            session.expire_date = timezone.now() + timedelta(seconds=settings.SESSION_COOKIE_AGE)
+            session.save()
+        return HttpResponse(json.dumps({"JWT_TOKEN": f"{token}"}, indent=2, separators=(',', ': ')),
+                            content_type="application/json", status=200)
+    return HttpResponse(json.dumps({"message": "Method not allowed"}, indent=2, separators=(',', ': ')),
+                        content_type="application/json", status=405)
